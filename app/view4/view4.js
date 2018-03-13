0a85b62ab968e91b1d244b8c482c6c9ae79f42ab
AleksanderOlsen91 <37067264+AleksanderOlsen91@users.noreply.github.com>
2018-03-13 09:03:41 +0100

    single book view
    First implementation:
    Added ID to books in JSON file

--- app/view4/view4.js
+++ app/view4/view4.js
@@ -3,13 +3,28 @@
 angular.module('myApp.view4', ['ngRoute'])
 
     .config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/view4', {
+        $routeProvider.when('/book/:id/:slug', {
             templateUrl: 'view4/view4.html',
             controller: 'View4Ctrl'
         });
     }])
 
-    .controller('View4Ctrl', ['shoppingCart', '$scope', function(shoppingCart, $scope) {
+    .controller('View4Ctrl', ['shoppingCart', '$scope', function(shoppingCart, $scope, $routeParams) {
+        console.log($routeParams.id);
+        console.log($routeParams.slug);
+
+        var id = parseInt($routeParams.id, 10);
+
+        books.load().then(function (listOfBooks) {
+            var allBooks = listOfBooks.data;
+            for(var book in allBooks) {
+                if (book.id === id) {
+                    $scope.book = book;
+                    break;
+                }
+            }
+        });
+
 
 
     }]);
